61c7fac3a83941fd468ded36981c7ad8f740b365
pranshu-kumar <pranshu0053@gmail.com>
2021-04-20 23:15:05 +0530

    regularization added

--- LogisticRegresssion/logistic_regression.py
+++ LogisticRegresssion/logistic_regression.py
@@ -7,10 +7,13 @@ from LogisticRegresssion.utils import *
 
 
 class LogisticRegressor():
-    def __init__(self, X=None, y=None):
+    def __init__(self, X=None, y=None, regularization='unreg', lambd=None):
         self.X = X
         self.y = y
 
+        self.regularization = regularization
+        if self.regularization == 'l1' or self.regularization == 'l2' :
+            self.lambd = lambd
         # self.X = self.X.astype(np.float128)
 
         self.y = np.reshape(y,(self.X.shape[0],1))
@@ -27,7 +30,46 @@ class LogisticRegressor():
 
         return W,b
 
+
     # Forward and Backward Propagation
+    def cost_function(self, A, n, W):
+        loss = -(1/n)*np.sum(self.y.T*np.log(A) + (1-self.y.T)*np.log(1-A))
+        
+        # Unregularized
+        if self.regularization == 'unreg':
+            return loss
+        
+        # L2 Regularization
+        elif self.regularization == 'l2':
+            reg = (self.lambd/(2*n)) * (W.T @ W) 
+
+        # L1 Regularization    
+        elif self.regularization == 'l1':
+            reg = (self.lambd/(2*n)) * abs(W)
+        
+        loss += reg
+        return loss
+
+    def compute_grad(self, A, W, n):
+
+        dw = (1/n) * (np.dot(self.X.T, (A-self.y.T).T))
+        db = (1/n) * (np.sum(A-self.y.T))
+        
+        # Unregularized
+        if self.regularization == 'unreg':
+            return dw, db
+
+        # L2 Regularization
+        elif self.regularization == 'l2':
+            dw += (self.lambd/n) * W
+
+        # L1 Regularization
+        elif self.regularization == 'l1':
+            dw += (self.lambd/n) * W
+
+        return dw, db
+
+            
     def forward_backward_prop(self, W, b):
         '''
         Function to implement Forward Propagation
@@ -39,15 +81,10 @@ class LogisticRegressor():
         A = sigmoid(np.dot(W,self.X.T)+b)
         
         # Unregularized
-        loss = -(1/n)*np.sum(self.y.T*np.log(A)) + (1-self.y.T)*np.log(1-A)
+        loss = self.cost_function(A, n, W)
+        # loss = -(1/n)*np.sum(self.y.T*np.log(A) + (1-self.y.T)*np.log(1-A))
 
-        dw = (1/n) * (np.dot(self.X.T, (A-self.y.T).T))
-        db = (1/n) * (np.sum(A-self.y.T))
-
-        # loss = np.squeeze(loss)
-        # print(np.squeeze(loss))
-
-        # assert(loss.shape == ())
+        dw, db = self.compute_grad(A, W, n)
 
         gradients = {
             'dW':dw,
@@ -100,12 +137,13 @@ class LogisticRegressor():
         return gradients, parameters
 
 
-    def predict(self, parameters, X, y):
+    def predict(self, parameters, X, y=None):
         '''
         Function to make predictions using the trained weights
 
         > Returns accuracy
         '''
+        # print(y)
         W = parameters['W']
         b = parameters['b']
         A = sigmoid(np.dot(W,X.T) + b)
@@ -122,9 +160,12 @@ class LogisticRegressor():
         
         y_hat = np.array(y_hat)
 
+        if y is None:
+            return y_hat
+
         acc = accuracy(y_hat, y)
 
-        print("Accuracy:", acc)
+        print("Test Accuracy:", acc)
 
         return y_hat
 
