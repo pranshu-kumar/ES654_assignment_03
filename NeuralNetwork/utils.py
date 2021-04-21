b481c1cb7010f56d9911b097f47b7cdc5f6fb666
pranshu-kumar <pranshu0053@gmail.com>
2021-04-22 02:38:22 +0530

    autograd added

---
+++ NeuralNetwork/utils.py
@@ -0,0 +1,15 @@
+def accuracy(y_hat, y):
+    '''
+    Function to print accuracy
+    '''
+    # print(y.shape, y_hat.shape)
+    assert(y_hat.shape == y.shape)
+
+    count = 0
+    for i in range(y.shape[1]):
+        if y_hat[0,i] == y[0,i]:
+            count += 1
+
+    # print(count)
+
+    return count/y.shape[1]
