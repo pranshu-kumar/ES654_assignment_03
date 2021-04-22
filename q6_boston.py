7425c28963f75c6080b90e6105ee295e4ed163e3
pranshu-kumar <pranshu0053@gmail.com>
2021-04-22 15:48:30 +0530

    q6 boston dataset done

--- q6_boston.py
+++ q6_boston.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import KFold
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 from NeuralNetwork.neural_net import NeuralNetwork
+import copy 
 
 # Get X and y
 X, y = load_boston(return_X_y=True)
@@ -12,7 +13,7 @@ X, y = load_boston(return_X_y=True)
 # Normalize Dataset
 
 scaler = MinMaxScaler()
-X = scaler.fit_transform(X)
+# X = scaler.fit_transform(X)
 y = y.reshape((1, len(y)))
 # y = scaler.fit_transform(y)
 
@@ -27,13 +28,24 @@ for train_index, test_index in kf.split(X):
     X_train,X_test = X[train_index], X[test_index]
     y_train, y_test = y[0,train_index], y[0,test_index]
 
+X_train = scaler.fit_transform(X_train)
 X_train = X_train.T
-X_test = X_test.T
-y_train = y_train.reshape((1, len(y_train)))
-y_train = scaler.fit_transform(y_train)
 
-y_test = y_test.reshape((1, len(y_test)))
-y_test = scaler.fit_transform(y_test)
+unscaled_X_test = copy.deepcopy(X_test)
+unscaled_X_test = unscaled_X_test.T
+
+X_test = scaler.fit_transform(X_test)
+X_test = X_test.T
+
+y_train = y_train.reshape((len(y_train),1))
+# y_train = scaler.fit_transform(y_train)
+y_train = y_train.T
+
+y_test = y_test.reshape((len(y_test),1))
+# y_test = scaler.fit_transform(y_test)
+y_test = y_test.T
+# print(y_test)
+
 
 print("X_train shape:", X_train.shape)
 print("y_train shape:", y_train.shape)
@@ -41,18 +53,19 @@ print("X_test shape:", X_test.shape)
 print("y_test shape:", y_test.shape)
 
 # Neural Network Model
-nn = NeuralNetwork(layer_info=[13, 5, 1], activations=['sigmoid', 'relu', 'sigmoid'])
-nn.train_model(X_train, y_train, verbose=True, plot_loss=False)
+nn = NeuralNetwork(layer_info=[13,5,1], activations=['sigmoid', 'relu', 'linear'])
+nn.train_model(X_train, y_train, verbose=True, plot_loss=False, num_iter=5000, learning_rate=0.001)
 
-y_hat = nn.predict(X_test)
+y_hat = nn.predict(X_test, y_test)
 
-y_hat = scaler.inverse_transform(y_hat)
-y_test = scaler.inverse_transform(y_test)
+# y_hat = scaler.inverse_transform(y_hat)
+# y_test = scaler.inverse_transform(y_test)
 
 # Calculate rmse
 sq_sum = 0
 for i in range(y_hat.shape[1]):
     sq_sum += (y_test[0,i] - y_hat[0,i])**2
+    print(y_test[0,i], y_hat[0,i])
 rmse = sq_sum/y_hat.shape[1]
 
 print("RMSE: ", rmse)
@@ -60,11 +73,20 @@ print("RMSE: ", rmse)
 
 y_hat = y_hat.reshape((y_hat.shape[1],))
 y_test = y_test.reshape((y_test.shape[1],))
-# print(range(1, y_hat.shape[1]+1, 1))
-# plt.hist2d()
-plt.plot(range(1, len(y_hat)+1, 1), y_hat, '--', label="Prediction")
-plt.plot(range(1, len(y_hat)+1, 1), y_test, alpha=0.7, label="Test Data")
+
+fig = plt.figure(figsize=(10,8))
+# plt.scatter(unscaled_X_test[0,:], y_hat, color='g',label="Prediction")
+# plt.scatter(unscaled_X_test[0,:], y_test, alpha=0.7, label="Test Data")
+# plt.title("Predicted Price vs CRIM\nRMSE:{}".format(rmse))
+# plt.xlabel("CRIM")
+
+plt.plot(np.squeeze(y_hat), '--', label="Prediction")
+plt.plot(np.squeeze(y_test), label="Test Data")
+plt.ylabel("Price")
+plt.xlabel("Samples")
 plt.legend()
 plt.grid()
-plt.title("RMSE:{}".format(rmse))
+plt.title("Predicted Price vs Sample\nRMSE:{}".format(rmse))
+# fig.savefig("pricevscrim.png")
+fig.savefig('pricevssample.png')
 plt.show()
